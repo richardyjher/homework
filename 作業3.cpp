b744930aa3a3f2177d2a74934471c8c84523a8d0
richardyjher <48574881+richardyjher@users.noreply.github.com>
2019-04-10 16:57:54 +0800

    Add files via upload

---
+++ 作業3.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	float high,weight,BMI;
+	printf("請輸入您的身高:");
+	scanf("%f",&high);
+	printf("請輸入您的體重:");
+	scanf("%f",&weight);
+	float x = high*high*0.0001;
+
+	printf("您的BMI是:%f\n",weight/x);
+	
+    system("pause");
+	return 0;
+}
